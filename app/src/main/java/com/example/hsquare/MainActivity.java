3b550530f90a17aff75db3246013ddd7f924d4cf
Usama Zahid <uzahid231@gmail.com>
2020-08-24 12:08:35 +0500

    Splash,AlreadyUser,Login done....

--- app/src/main/java/com/example/hsquare/MainActivity.java
+++ app/src/main/java/com/example/hsquare/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.hsquare;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button btn_join, btn_already;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_join=findViewById(R.id.btn_main_join);
+        btn_already=findViewById(R.id.btn_main_already);
+
+        btn_already.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,LoginAcitvity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hsquare/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.hsquare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    int SPLASH_TIME = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mySuperIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mySuperIntent);
+
+            }
+        }, SPLASH_TIME);
+    }
+}
